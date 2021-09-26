f250bb54ba6db823c8323cbf5edbe17b280291e3
malapatisaiteja <malapatisaiteja@gmail.com>
2021-09-26 14:48:25 -0400

    Modularized Ranker Class and Oracle Class

--- whun.py
+++ whun.py
@@ -15,6 +15,8 @@ from whun_helper.Item import Item
 sys.path.append('/whun_helper')
 from whun_helper.SATSolver import SATSolver
 from whun_helper.Search import Search
+from whun_helper.Ranker import Ranker
+from whun_helper.Oracle import Oracle
 
 
 # SETUP VARIABLES
@@ -27,134 +29,6 @@ NUM_FEATURES = 108
 HUMAN_WEIGHT = 1.5
 
 random.seed(datetime.now())
-
-
-class Oracle:
-    def __init__(self, size):
-        self.picked = [0] * size
-
-    def pick(self, q_idx, node):
-        west_points = 0
-        east_points = 0
-        # Check how many of these questions I have picked before
-        for i in range(len(q_idx)):
-            if node.east[0].item[q_idx[i]] and self.picked[q_idx[i]] == 1:
-                east_points += 1
-            elif node.west[0].item[q_idx[i]] and self.picked[q_idx[i]] == 1:
-                west_points += 1
-        # Random selection favoring the side i like the most
-        if east_points + west_points > 0:
-            weighted_selection = west_points / (east_points + west_points)
-        else:
-            weighted_selection = 0.5
-
-        if random.random() < weighted_selection:
-            selected = 0
-        else:
-            selected = 1
-        # Update my vector of picked options
-        if selected:
-            for i in range(min(len(q_idx), 4)):
-                if self.picked[q_idx[i]] == 0:
-                    self.picked[q_idx[i]] = node.east[0].item[q_idx[i]]
-        else:
-            for i in range(min(len(q_idx), 4)):
-                if self.picked[q_idx[i]] == 0:
-                    self.picked[q_idx[i]] = node.west[0].item[q_idx[i]]
-        # Return selected {0 = East, 1 = West}
-        return selected
-
-
-class Ranker:
-
-    @staticmethod
-    def level_rank_features(root, weights):
-        if not root:
-            return
-        items_rank = np.zeros(len(root.west[0].item))
-        q = [[root, 1]]
-        while len(q):
-            p = q[0]
-            q.pop(0)
-            if p[0].west is not None and p[0].east is not None:
-                diff = p[0].diff_array()
-                for i, d in enumerate(diff):
-                    if d and items_rank[i] == 0:
-                        items_rank[i] = p[1] * weights[i]
-            if p[0].west_node:
-                q.append([p[0].west_node, p[1] + 1])
-            if p[0].east_node:
-                q.append([p[0].east_node, p[1] + 1])
-        print(int(np.sum([1 for x in items_rank if x > 0])),
-              "Total number of important questions")
-        return items_rank
-
-    @staticmethod
-    def rank_nodes(root, rank):
-        if not root:
-            return
-        largest = -100000000
-        q = [[root, 1]]
-        while len(q):
-            p = q[0]
-            q.pop(0)
-            if p[0].west is not None and p[0].east is not None:
-                diff = p[0].diff_array()
-                res = np.multiply(diff, rank)
-                p[0].score = (np.sum(res) * p[0].weight) / np.sum(diff)
-                if p[0].score > largest:
-                    largest = p[0].score
-            if p[0].west_node:
-                q.append([p[0].west_node, p[1] + 1])
-            if p[0].east_node:
-                q.append([p[0].east_node, p[1] + 1])
-        return largest
-
-    @staticmethod
-    def pr_level(root):
-        tree_lvl = []
-        if not root:
-            return
-        q = [[root, 1]]
-        while len(q):
-            p = q[0]
-            q.pop(0)
-            if p[0].west is not None and p[0].east is not None:
-                tree_lvl.append((p[1], p[0].TreeNode.difference()))
-            if p[0].west_node:
-                q.append([p[0].west_node, p[1] + 1])
-            if p[0].east_node:
-                q.append([p[0].east_node, p[1] + 1])
-        return tree_lvl
-
-    @staticmethod
-    def check_solution(root):
-        count = 0
-        if not root:
-            return
-        q = [[root, 1]]
-        while len(q):
-            p = q[0]
-            q.pop(0)
-            if p[0].weight == 1 and p[0].leaf:
-                count += 1
-            if p[0].west_node:
-                q.append([p[0].west_node, p[1] + 1])
-            if p[0].east_node:
-                q.append([p[0].east_node, p[1] + 1])
-        print("Count =", count)
-        if count == 1:
-            return 1
-        if count < 1:
-            return -1
-        return None
-
-
-
-
-
-
-
 
 
 def split_bin(items, total_group):
---
+++ whun_helper/Oracle.py
@@ -0,0 +1,37 @@
+import random
+
+
+class Oracle:
+    def __init__(self, size):
+        self.picked = [0] * size
+
+    def pick(self, q_idx, node):
+        west_points = 0
+        east_points = 0
+        # Check how many of these questions I have picked before
+        for i in range(len(q_idx)):
+            if node.east[0].item[q_idx[i]] and self.picked[q_idx[i]] == 1:
+                east_points += 1
+            elif node.west[0].item[q_idx[i]] and self.picked[q_idx[i]] == 1:
+                west_points += 1
+        # Random selection favoring the side i like the most
+        if east_points + west_points > 0:
+            weighted_selection = west_points / (east_points + west_points)
+        else:
+            weighted_selection = 0.5
+
+        if random.random() < weighted_selection:
+            selected = 0
+        else:
+            selected = 1
+        # Update my vector of picked options
+        if selected:
+            for i in range(min(len(q_idx), 4)):
+                if self.picked[q_idx[i]] == 0:
+                    self.picked[q_idx[i]] = node.east[0].item[q_idx[i]]
+        else:
+            for i in range(min(len(q_idx), 4)):
+                if self.picked[q_idx[i]] == 0:
+                    self.picked[q_idx[i]] = node.west[0].item[q_idx[i]]
+        # Return selected {0 = East, 1 = West}
+        return selected
