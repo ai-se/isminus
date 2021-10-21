d34ba651a8588b25aa53478b7103c9a1f6e3519a
Akhil Gangarpu Sudhakar <akhil.clk93@gmail.com>
2021-10-21 18:09:15 -0400

    Updates file name in run_sa.sh

--- run_sa.sh
+++ run_sa.sh
@@ -1,3 +1,4 @@
+[ -d sa_reports ] || mkdir sa_reports
 pylint ./whun_helper/input_output.py > sa_reports/input_output_sa.txt
 pylint whun_helper/item.py > sa_reports/item_sa.txt
 pylint whun_helper/Method. --pyextension-pkg-whitelist=math > sa_reports/Method_sa.txt
@@ -5,5 +6,5 @@ pylint whun_helper/oracle.py > sa_reports/oracle_sa.txt
 pylint whun_helper/ranker.py > sa_reports/ranker_sa.txt
 pylint whun_helper/sat_solver.py > sa_reports/sat_solver_sa.txt
 pylint whun_helper/search.py > sa_reports/search_sa.txt
-pylint whun_helper/TreeNode.py > sa_reports/TreeNode_sa.txt
+pylint whun_helper/tree_node.py > sa_reports/tree_node_sa.txt
 pylint whun.py > sa_reports/whun_sa.txt
