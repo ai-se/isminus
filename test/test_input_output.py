0a2dbb20dffc111b8e8b2ab5080c341e83aa0dfd
malapatisaiteja <malapatisaiteja@gmail.com>
2021-11-03 04:46:42 -0400

    Removed unwanted print statement

--- test/test_input_output.py
+++ test/test_input_output.py
@@ -11,6 +11,5 @@ def test_get_question_text():
     list_expected = ['a', 'b', 'c', 'd']
     with open('test_resources/question.csv', 'r') as file:
         list_actual = InputOutput.get_question_text(file, 'question')
-        print(list_actual)
         t = TestCase()
         t.assertCountEqual(list_actual, list_expected)
