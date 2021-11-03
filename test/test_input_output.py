776c52227b0528e1a62b1940c4c2bb4fc11607d4
jpatel33 <jayp.jd7@gmail.com>
2021-11-03 05:19:31 -0400

    fixing the test resource file accessing

--- test/test_input_output.py
+++ test/test_input_output.py
@@ -5,11 +5,11 @@ from unittest import TestCase
 cur_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(cur_dir)
 from whun_helper.input_output import InputOutput
-
+print(sys.path)
 
 def test_get_question_text():
     list_expected = ['a', 'b', 'c', 'd']
-    with open('test_resources/question.csv', 'r') as file:
+    with open('test/test_resources/question.csv', 'r') as file:
         list_actual = InputOutput.get_question_text(file, 'question')
         t = TestCase()
         t.assertCountEqual(list_actual, list_expected)
