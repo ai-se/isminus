e4c0231e64973262d7a17cbae9c704f0e77b175b
vijdurk <vijayadurgakona91@gmail.com>
2021-11-03 11:48:12 -0400

    SEWE-76

---
+++ test/test_treenode.py
@@ -0,0 +1,30 @@
+import unittest
+
+import numpy as np
+
+from whun_helper.item import Item
+from whun_helper.tree_node import tree_node
+
+
+class test_tree_node(unittest.TestCase):
+
+    def test_difference(self):
+        item1 = Item(
+            [0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1,
+             0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0,
+             0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0,
+             0], [0, 1, 0, 0, 0, 0])
+        item2 = Item(
+            [1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1,
+             0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0,
+             0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0,
+             0], [0, 1, 0, 0, 0, 0])
+        east_item = [item1, [1, 2], [3, 4]]
+        west_item = [item2, [1, 2], [3, 4]]
+        node = tree_node(east_item, west_item, None, None, None)
+        result_expected = 1  # difference between items is 1 in the first position
+        self.assertEqual(node.difference(), result_expected)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
