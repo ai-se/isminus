b34e616f146b3b52bd3293333552ee4ce6806e73
malapatisaiteja <malapatisaiteja@gmail.com>
2021-11-03 04:45:36 -0400

    Fixed test case failure in test_input_output file

--- test/test_input_output.py
+++ test/test_input_output.py
@@ -1,11 +1,16 @@
-import sys,os
+import os
+import sys
 from unittest import TestCase
+
 cur_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(cur_dir)
 from whun_helper.input_output import InputOutput
+
 
 def test_get_question_text():
     list_expected = ['a', 'b', 'c', 'd']
     with open('test_resources/question.csv', 'r') as file:
         list_actual = InputOutput.get_question_text(file, 'question')
-        TestCase.assertCountEqual(list_actual, list_expected)
+        print(list_actual)
+        t = TestCase()
+        t.assertCountEqual(list_actual, list_expected)
