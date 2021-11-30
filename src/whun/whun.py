ad45498fd1b76a1d4c883f175869c4c84044587d
Akhil Gangarpu Sudhakar <akhil.clk93@gmail.com>
2021-11-30 16:36:57 -0500

    Adds flag to handle Oracle or Human input

--- src/whun/whun.py
+++ src/whun/whun.py
@@ -121,22 +121,23 @@ def prepare_result_label(method_obj):
 
 
 def whun_run(file_names, eval_files, is_oracle_enabled=True):
-    app = QApplication(sys.argv)
-    app.setApplicationName('WhunWindow')
-    ui_obj = UIHelper(app, init_process)
-    ui_obj.show()
-    sys.exit(app.exec())
+    if not is_oracle_enabled:
+        global ui_obj
+        app = QApplication(sys.argv)
+        app.setApplicationName('WhunWindow')
+        ui_obj = UIHelper(app, init_process, file_names, eval_files, is_oracle_enabled)
+        ui_obj.show()
+        sys.exit(app.exec())
+    else:
+        init_process(['Scrum10k.csv'], ['flight_eval.csv'], is_oracle_enabled)
 
 def init_process(file_names, eval_files, is_oracle_enabled=True):
     for file, e_file in zip(file_names, eval_files):
         main(file, e_file, is_oracle_enabled)
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    app.setApplicationName('WhunWindow')
-    ui_obj = UIHelper(app, init_process)
-    ui_obj.show()
-    sys.exit(app.exec())
+    whun_run(['Scrum10k.csv'], ['flight_eval.csv'], False)
+    # whun_run(['Scrum10k.csv'], ['flight_eval.csv'], True)
 
 
 
